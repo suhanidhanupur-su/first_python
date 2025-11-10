ce7bd970f519f273e4e1ac478c9d60771f84ae33
suhanidhanupur-su <suhanidhanupur@gmail.com>
2025-11-10 23:35:30 +0530

    update

--- suhani.py
+++ suhani.py
@@ -1,1 +1,58 @@
-print("hellow world")
+print("hellow world")#string
+
+str1 = 'Tridev Sharma'
+str2 = "Tridev Sharma"
+str3 = """Tridev Sharma"""
+
+print(str1 + " " + str2)
+print(len(str1 + str2 + str3)) #length
+
+print(str1[0 :len (str1)]) #slicing
+
+print(str1.endswith("ma")) #checking
+
+print(str2.capitalize()) #capitalize
+
+print(str3.replace("Tridev" , "Prem")) #Replacing
+
+print(str1.find('v')) 
+
+print(str2.count('a'))
+
+#conditional Statment
+
+a = int(input("First No "))
+b = int(input("Second No "))
+
+s = (input("Choose Simble "))
+
+if(s == "*"):
+    print(a*b)
+
+elif(s == "+"):
+    print(a+b)
+
+elif(s == "-"):
+    print(a-b)
+
+elif(s == "/"):
+    print(a/b)
+
+elif(s == "%"):
+    print((a/b)*100)
+
+else:
+    print("Unknown Simble")
+
+mark =  int(input("Marks --> ",  )) 
+
+if(mark >= 90):
+    Mark = "A"
+elif(mark >= 80 and mark < 90 ):
+    mark = "B"
+elif(mark >= 70 and mark <= 80):
+    mark = "C"
+elif(mark <= 69):
+    mark = "D"
+
+print(mark)
